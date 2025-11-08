5abb22173131e68d9c99d5ed30d41931ee6856f4
wk-y <86206045+wk-y@users.noreply.github.com>
2025-11-07 19:03:40 -0800

    Snake: Working prototype :D

--- MinigameFramework/minigames/snake/player.gd
+++ MinigameFramework/minigames/snake/player.gd
@@ -2,8 +2,7 @@ extends CharacterBody2D
 
 const BODY = preload("uid://ca7yc3yllp57a")
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 700.0
 
 
 @onready var tail: CharacterBody2D = $"../Body"
@@ -14,11 +13,12 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = SPEED * direction / max(direction.length(), 1)
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		var new_tail: SnakeMinigameBody = BODY.instantiate()
-		get_parent().add_child(new_tail)
-		new_tail.previous_segment = tail
-		new_tail.global_position = tail.global_position
-		tail = new_tail
-		
 	move_and_slide()
+
+
+func _on_snake_pickup_gotten(current_count: int) -> void:
+	var new_tail: SnakeMinigameBody = BODY.instantiate()
+	get_parent().add_child(new_tail)
+	new_tail.previous_segment = tail
+	new_tail.global_position = tail.global_position
+	tail = new_tail
--- MinigameFramework/minigames/snake/snake.gd
+++ MinigameFramework/minigames/snake/snake.gd
@@ -2,8 +2,13 @@ extends Minigame
 
 const PICKUP = preload("uid://1x1kcdxmvc3f")
 
+signal pickup_gotten(current_count:int)
+
 @onready var top_left: Node2D = $TopLeft
 @onready var bottom_right: Node2D = $BottomRight
+
+@export var pickup_target:int = 3
+var pickups_gotten:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,4 +35,14 @@ func _spawn_pickup() -> void:
 			bottom_right.global_position.y,
 			randf())
 	
-	pickup.picked_up.connect(_spawn_pickup)
+	pickup.picked_up.connect(_on_pickup_picked_up)
+
+
+func _on_pickup_picked_up() -> void:
+	pickups_gotten += 1
+	pickup_gotten.emit(pickups_gotten)
+	
+	if pickups_gotten >= pickup_target:
+		win()
+
+	_spawn_pickup()
