41100c66baf8f5c0ebb2534340380238bbbf8523
Aditya Sharma <aditya.sharma7202@gmail.com>
2025-11-25 19:06:01 -0800

    spawning multiple balls based on difficulty

--- MinigameFramework/minigames/pong_defender/pong_defender.gd
+++ MinigameFramework/minigames/pong_defender/pong_defender.gd
@@ -6,12 +6,15 @@ func start() -> void:
 	_spawn_balls()
 
 func _spawn_balls() -> void:
-	for i in range(3):
+	var _num_balls:int = 3
+	print((difficulty - 1) / 0.1)
+	_num_balls += ((difficulty - 1) / 0.1) as int
+	for i in range(_num_balls):
 		var ball: Ball = _ball_scene.instantiate() as Ball
 		print(ball.get_script())
 		add_child(ball)
 		var _x_pos :float = randf_range(100, 1200)
 		var _y_pos :float = randf_range(65, 200)
 		ball.global_position = Vector2(_x_pos, _y_pos)
-		ball.set_vel(randf_range(-100, 100), randf_range(100, 300))
+		ball.set_vel(randf_range(-100, 100), randf_range(200, 300))
 	
