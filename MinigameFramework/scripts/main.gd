00c7571927509ff1736e59c2784911f2fd9f50ca
BuilderRobot <jake@stellyfamily.com>
2025-11-26 18:36:50 -0800

    collection bug

--- MinigameFramework/scripts/main.gd
+++ MinigameFramework/scripts/main.gd
@@ -2,6 +2,11 @@ class_name Main
 extends Node
 
 func _ready() -> void:
+	#var scene = load("res://minigames/homerun/homerun.tscn")
+	#var instance = scene.instantiate()
+	#add_child(instance)
+	#print("Instance added, inside tree?", instance.is_inside_tree())
+	
 	GameManager.main_scene = self
 	GameManager.switch_to_world()
 	GameManager.world_manager.load_ui("Menu")
