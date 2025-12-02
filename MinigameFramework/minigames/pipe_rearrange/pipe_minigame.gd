5e599f9d4c1421a29adbac83567486fe3914bdee
DrewCoding <drewwilliams9090@gmail.com>
2025-12-01 17:30:11 -0800

    Added fanfare to winning

--- MinigameFramework/minigames/pipe_rearrange/pipe_minigame.gd
+++ MinigameFramework/minigames/pipe_rearrange/pipe_minigame.gd
@@ -4,6 +4,9 @@ var pipes : Array[Pipe] = []
 var pipes_out_of_order = 5
 var all_connect: int = 0
 
+@onready var celeb_particles_tl: GPUParticles2D = $CelebrationParticleTL
+@onready var celeb_particles_tr: GPUParticles2D = $CelebrationParticleTR
+
 func start():
 	# im getting all the pipes into a list and returning to their default
 	for pipe in get_children():
@@ -11,7 +14,7 @@ func start():
 			pipes.append(pipe)
 			pipe.rotation_degrees = pipe.correct_rotation
 	
-	countdown_time /= difficulty
+	countdown_time = 7 / difficulty
 	
 	# im getting random pipes and scrambling them
 	for i in range(pipes_out_of_order):
@@ -23,6 +26,8 @@ func _process(_delta):
 
 
 func win():
+	celeb_particles_tl.emitting = true
+	celeb_particles_tr.emitting = true
 	super()
 
 
@@ -32,6 +37,5 @@ func pipe_connect_stats():
 	for pipe in pipes:
 		if pipe._is_correct_rotation:
 			all_connect += 1
-	print(all_connect, " Sdsdssd ", len(pipes))
 	if all_connect == len(pipes):
 		win()
