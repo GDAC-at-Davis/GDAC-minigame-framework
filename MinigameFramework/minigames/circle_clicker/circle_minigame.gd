787516b4d6526e55a8dea8c677567c211ba9f95e
Chiamin Yeh <manzanita1002000@gmail.com>
2026-01-05 19:57:23 -0800

    Added sfx and a new shape

--- MinigameFramework/minigames/circle_clicker/circle_minigame.gd
+++ MinigameFramework/minigames/circle_clicker/circle_minigame.gd
@@ -1,9 +1,11 @@
 extends Minigame
+@onready var sfx_square: AudioStreamPlayer = $sfx_circle/sfx_square
+@onready var sfx_circle: AudioStreamPlayer = $sfx_circle
 
 # Preload scenes
 var circle_scene: PackedScene = preload("res://minigames/circle_clicker/circle.tscn")
 var square_scene: PackedScene = preload("res://minigames/circle_clicker/square.tscn")
-
+var triangle_scene: PackedScene = preload("res://minigames/circle_clicker/triangle.tscn")
 # Game State
 var circles_popped: int = 0
 var total_circles: int = 0
@@ -22,39 +24,43 @@ func start():
 	
 	for i in range(object_count):
 		spawn_shape()
-		#
-	## If RNG gave us 0 circles, spawn at least one so the game is playable
-	#if total_circles == 0:
+
 	spawn_single_circle()
 
 func spawn_shape():
 	var shape: Area2D
 	var screen_size = get_viewport().get_visible_rect().size
 	
+	
 	## 50% chance for triangle or square
 	if randf() > 0.5:
 		shape = square_scene.instantiate()
+		shape.modulate = Color(randf(), randf(), randf())
 		#total_circles += 1 # Count the circle!
 	else:
-		shape = square_scene.instantiate()
+		shape = triangle_scene.instantiate()
+		shape.modulate = Color(randf(), randf(), randf())
+		
 	
 	add_child(shape)
 	# Random position with padding so they don't spawn on the edge
 	shape.global_position = Vector2(
 		randf_range(60, screen_size.x - 60),
-		randf_range(60, screen_size.y - 60)
+		randf_range(60, screen_size.y - 100)
 	)
 
 func spawn_single_circle():
 	var shape = circle_scene.instantiate()
+	shape.modulate = Color(randf(), randf(), randf())
 	total_circles += 1
 	add_child(shape)
 	var screen_size = get_viewport().get_visible_rect().size
 	shape.global_position = Vector2(randf_range(60, screen_size.x - 60),
-		randf_range(60, screen_size.y - 60))
+		randf_range(60, screen_size.y - 100))
 
 # Called by circle.gd
 func circle_clicked():
+	sfx_circle.play()
 	circles_popped += 1
 	# Check win condition
 	if circles_popped >= total_circles:
@@ -62,9 +68,12 @@ func circle_clicked():
 
 # Called by square.gd
 func square_clicked():
-	# Force the minigame to end/fail if a square is hit
-	# If you don't have a lose() function, we can just print for now
+	sfx_square.play()
 	print("Hit a square!") 
+	
+func trig_clicked():
+	sfx_square.play()
+	print("Hit a triangle!") 
 
 func win():
 	super() # Call the parent win function
