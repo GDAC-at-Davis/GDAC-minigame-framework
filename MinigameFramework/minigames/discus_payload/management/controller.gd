c15d6f533ea82a9ed31c3c86e603bf00980133e0
SamuelDMFerreira <sam.ferreira@outlook.com>
2026-04-04 11:43:02 -0700

    add payload spinning and some spin game feel

--- MinigameFramework/minigames/discus_payload/management/controller.gd
+++ MinigameFramework/minigames/discus_payload/management/controller.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var player : RigidBody2D
+@export var payload : RigidBody2D
 
 ## track if holding mouse 
 var holding_mouse : bool 
@@ -8,14 +9,35 @@ var holding_mouse : bool
 ## track the position of the mouse 
 var mouse_pos : Vector2 
 
+## tracks how long the player has been holding down the position
+var time_held_down : float 
+
 func _physics_process(delta: float) -> void:
 	if not holding_mouse and Input.is_action_just_pressed("primary"):
 		holding_mouse = true
+		time_held_down = 0 
+		# do this to prevent bug where detect movement on click
+		mouse_pos = get_viewport().get_mouse_position()
 	elif holding_mouse and Input.is_action_just_released("primary"):
 		holding_mouse = false
 		
+		# lauch the payload
+		var direction : Vector2 = player.find_child("HoldPosition").global_position - player.global_position
+		payload.apply_impulse(player.angular_velocity * direction.normalized() * 20)
+		
 	if holding_mouse:
+		# decide speed 
 		var new_mouse_pos : Vector2 = get_viewport().get_mouse_position()
 		var change_in_mouse_pos := new_mouse_pos - mouse_pos
 		mouse_pos = new_mouse_pos
-		player.apply_torque(mouse_pos.length() * player.speed)
+		
+		var mouse_pos_to_torque = player.torque_curve.sample(change_in_mouse_pos.length())
+		var time_mod = player.time_modifier.sample(min(2.0, time_held_down))
+		
+		player.apply_torque(change_in_mouse_pos.length() * mouse_pos_to_torque * time_mod * player.max_speed)
+		
+		# move the payload
+		payload.global_position = player.find_child("HoldPosition").global_position
+		
+		# increase time 
+		time_held_down += delta
--- MinigameFramework/minigames/discus_payload/player/player.gd
+++ MinigameFramework/minigames/discus_payload/player/player.gd
@@ -1,3 +1,10 @@
 extends RigidBody2D
 
-@export var speed : float
+## the max torque that is applied 
+@export var max_speed : float
+
+## the curve modifying the torque based on distance mouse travels 
+@export var torque_curve : Curve
+
+## the curve that modifyis the torque based on how long the player held down the button
+@export var time_modifier : Curve
