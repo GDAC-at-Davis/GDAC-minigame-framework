d70175f543b332a714cd995fc5889dd622706d27
sshelei <slolei@ucdavis.edu>
2026-04-07 16:33:09 -0700

    Boxer shows uppp and punches weeeee

--- MinigameFramework/minigames/shadowboxing/shadowboxing_minigame.gd
+++ MinigameFramework/minigames/shadowboxing/shadowboxing_minigame.gd
@@ -1,6 +1,7 @@
 extends Minigame
 
 var arrow_packed_scene: PackedScene = preload("res://minigames/shadowboxing/arrow.tscn")
+var boxer_scene: PackedScene = preload("res://minigames/shadowboxing/boxer.tscn")
 var arrows: Array[Area2D]
 var direction: int = 0
 var move_weight: float = 0.0
@@ -11,7 +12,7 @@ var speed: float = 0.2
 #@export var movement_curve: Curve
 
 # 0: up, 1: right, 2: down, 3: left (think of compass, never eat soggy waffles!)
-
+@onready var boxer_sprite = $Boxer/AnimatedSprite2D
 @onready var particles: GPUParticles2D = $BalloonParticles
 
 func start():
@@ -20,12 +21,15 @@ func start():
 	# Adjust for difficulty
 	countdown_time /= difficulty
 	delay /= difficulty
+	
+	await get_tree().create_timer(1.5).timeout
+	#await get_tree().create_timer(delay).timeout
 	# Spawn the arrow
-	await get_tree().create_timer(delay).timeout
 	var arrow: Area2D = arrow_packed_scene.instantiate()
 	add_child(arrow)
 	arrows.append(arrow)
-	direction = randi_range(0, 3)
+	direction = 3
+	#direction = randi_range(0, 3)
 	if direction == 0:
 		arrows[0].global_position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2 - 200)
 		arrows[0].rotation_degrees = 270
@@ -36,6 +40,7 @@ func start():
 		arrows[0].global_position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2 + 200)
 		arrows[0].rotation_degrees = 90
 	elif direction == 3:
+		boxer_sprite.play("left_punch")
 		arrows[0].global_position = Vector2(get_viewport().size.x/2 - 400, get_viewport().size.y/2)
 		arrows[0].rotation_degrees = 180
 	# start timer for blinks
@@ -54,7 +59,9 @@ func run():
 			arrows[0].global_position += Vector2(0, speed)
 		elif direction == 3:
 			arrows[0].global_position -= Vector2(speed, 0)
-			
+			boxer_sprite.pause()
+			await get_tree().create_timer(0.001, false).timeout
+			boxer_sprite.play("left_punch")
 		if Input.is_action_pressed(&"up"):
 			remove_arrow(arrows)
 			if (direction != 0):
