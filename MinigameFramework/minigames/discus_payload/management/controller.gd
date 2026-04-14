94fd21a909c9df0014bf1f8f3e13d0014eb162ba
SamuelDMFerreira <sam.ferreira@outlook.com>
2026-04-13 17:11:21 -0700

    got rotation on mouse

--- MinigameFramework/minigames/discus_payload/management/controller.gd
+++ MinigameFramework/minigames/discus_payload/management/controller.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var player : RigidBody2D
+@export var player : CharacterBody2D
 @export var payload : RigidBody2D
 
 ## track if holding mouse 
@@ -8,6 +8,7 @@ var holding_mouse : bool
 
 ## track the position of the mouse 
 var mouse_pos : Vector2 
+var direction_of_mouse : Vector2
 
 ## tracks how long the player has been holding down the position
 var time_held_down : float 
@@ -16,28 +17,31 @@ func _physics_process(delta: float) -> void:
 	if not holding_mouse and Input.is_action_just_pressed("primary"):
 		holding_mouse = true
 		time_held_down = 0 
+		# lock the player onto the mouse position 
+		#"""
+		var new_mouse_pos : Vector2 = get_viewport().get_mouse_position()
+		var hold_pos : Vector2 = player.get_node("HoldPosition").global_position
+		var new_direction_of_mouse : Vector2 = new_mouse_pos - player.global_position
+		var angle_of_rotation : float = acos(hold_pos.dot(new_direction_of_mouse) / (new_direction_of_mouse.length() * hold_pos.length()))
+		if not is_nan(angle_of_rotation):
+			player.rotate(angle_of_rotation)#angle_of_rotation)
+		print(angle_of_rotation)
+		#"""
+		
 		# do this to prevent bug where detect movement on click
 		mouse_pos = get_viewport().get_mouse_position()
 	elif holding_mouse and Input.is_action_just_released("primary"):
 		holding_mouse = false
 		
 		# lauch the payload
-		var direction : Vector2 = player.find_child("HoldPosition").global_position - player.global_position
-		payload.apply_impulse(player.angular_velocity * direction.normalized() * 20)
+		#var direction : Vector2 = player.find_child("HoldPosition").global_position - player.global_position
+		#payload.apply_impulse(player.angular_velocity * direction.normalized() * 20)
 		
 	if holding_mouse:
-		# decide speed 
 		var new_mouse_pos : Vector2 = get_viewport().get_mouse_position()
-		var change_in_mouse_pos := new_mouse_pos - mouse_pos
-		mouse_pos = new_mouse_pos
-		
-		var mouse_pos_to_torque = player.torque_curve.sample(change_in_mouse_pos.length())
-		var time_mod = player.time_modifier.sample(min(2.0, time_held_down))
-		
-		player.apply_torque(change_in_mouse_pos.length() * mouse_pos_to_torque * time_mod * player.max_speed)
-		
-		# move the payload
-		payload.global_position = player.find_child("HoldPosition").global_position
-		
-		# increase time 
-		time_held_down += delta
+		var new_direction_of_mouse : Vector2 = new_mouse_pos - player.global_position
+		#var angle_of_rotation : float = acos(new_direction_of_mouse.dot(direction_of_mouse) / (new_direction_of_mouse.length() * direction_of_mouse.length()))
+		var angle_of_rotation : float = acos(direction_of_mouse.dot(new_direction_of_mouse) / (new_direction_of_mouse.length() * direction_of_mouse.length()))
+		if not is_nan(angle_of_rotation):
+			player.rotate(angle_of_rotation)#angle_of_rotation)
+		direction_of_mouse = new_direction_of_mouse
