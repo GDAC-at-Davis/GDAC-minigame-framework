27351907fa704a20d97a6e0bc543f1efca328510
SamuelDMFerreira <sam.ferreira@outlook.com>
2026-05-08 13:17:20 -0700

    made win and lose tetx

--- MinigameFramework/minigames/discus_payload/discus_payload.gd
+++ MinigameFramework/minigames/discus_payload/discus_payload.gd
@@ -1,7 +1,20 @@
 extends Minigame
 
 @export var controller : Node
+@export var win_distance : float
 
 func _ready():
+	super._ready()
 	controller.payload.defeated.connect(func() : lose())
 	
+func _process(delta: float) -> void:
+	if controller.payload.global_position.distance_to(controller.player.global_position) > win_distance:
+		win()
+	
+func win() -> void:
+	$win.visible = true
+	super.win()
+	
+func lose() -> void:
+	$lose.visible = true
+	super.lose()
